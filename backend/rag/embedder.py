3fa17d0cd426f3aa9dbcc78cdde9cff273635de5
김현용 <pak101044@gmail.com>
2026-04-13 16:36:54 +0900

    chromaDB 적재 및 docker 연결 실행 [docker-compose up -d db chromadb]

--- backend/rag/embedder.py
+++ backend/rag/embedder.py
@@ -7,3 +7,16 @@
 # TODO: [R4] 구현
 # - SentenceTransformer("all-MiniLM-L6-v2") 초기화
 # - encode() 래퍼
+
+import torch
+from sentence_transformers import SentenceTransformer
+from chromadb.api.types import EmbeddingFunction, Documents, Embeddings
+
+class MiniLMEmbeddingFunction(EmbeddingFunction):
+    def __init__(self):
+        # device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = SentenceTransformer("all-MiniLM-L6-v2", device="cpu")
+
+    def __call__(self, input: Documents) -> Embeddings:
+        embeddings = self.model.encode(input).tolist()
+        return embeddings
